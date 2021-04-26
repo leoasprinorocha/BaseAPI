8e93cdf26fdd41e9098d8d6c4a2f4822c8e9e29d
leoasprinorocha <asprinoetecita@gmail.com>
2021-04-26 11:04:28 -0400

    First commit

---
+++ BaseAPI.DATA/Context/BaseAPIContext.cs
@@ -0,0 +1,30 @@
+﻿using BaseAPI.DOMAIN.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseAPI.DATA.Context
+{
+    public class BaseAPIContext : DbContext
+    {
+        public BaseAPIContext(DbContextOptions<BaseAPIContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>().
+                HasData(
+                new Cliente { ID = 1, Nome = "Leonardo Asprino Rocha" }
+
+                );
+        }
+
+    }
+}
---
+++ BaseAPI/Migrations/20210426145141_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseAPI.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Clientes",
+                columns: new[] { "ID", "Nome" },
+                values: new object[] { 1, "Leonardo Asprino Rocha" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
